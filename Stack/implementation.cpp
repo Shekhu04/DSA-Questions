7c5dd4c021292a7ca50e13c5f68175399718bfb1
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2023-01-24 09:53:49 +0530

    Create implementation.cpp

---
+++ Stack/implementation.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+
+    //creation of stack
+    stack <int> s;
+
+    //Inserting element in stack
+    s.push(4);
+    s.push(7);
+
+    //deleting element from stack
+    s.pop();
+
+    cout<< "Printing top element : " << s.top() << endl;
+
+    if(s.empty()){
+        cout << "Stack is empty" << endl;
+    }
+    else{
+        cout << " Stack is not empty" << endl;
+    }
+
+    return 0;
+}
