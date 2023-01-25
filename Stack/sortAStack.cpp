6b6ce20f0ae2a8817ad5534598fa22bde39bd8c3
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2023-01-25 15:09:28 +0530

    Update sortAStack.cpp

--- Stack/sortAStack.cpp
+++ Stack/sortAStack.cpp
@@ -2,25 +2,27 @@
 void sortedInsert(stack<int> &stack , int num)
 {
 	//base case
-	if(stack.empty() || (!stack.empty() && stack.top() < num) ){
-		stack.push(num);
+	if(stack.empty() || (!stack.empty() && stack.top() < num) )  //if stack is empty then insert element directly and 
+		                                                    //if element at top of stack is less than the number then insert element at top of the stack
+	{
+		stack.push(num);  //insert number
 		return;
 	}
 
-	int n = stack.top();
-	stack.pop();
+	int n = stack.top();  
+	stack.pop();        //storing and removing top element of stack in n
 
 	//recursive call
 	sortedInsert(stack,num);
 
-	stack.push(n);
+	stack.push(n);  //inserting n in sorted manner
 }
 
 void sortStack(stack<int> &stack)
 {
 	// Write your code here
 	//base case
-	if(stack.empty())
+	if(stack.empty())  
 	return;
 
 	int num = stack.top();
