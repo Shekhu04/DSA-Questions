636c65ee0991eafb32e4470beaab4d43889728f4
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2023-02-02 11:51:20 +0530

    Create BSTInsertionInPreorder.cpp

---
+++ Tree/BSTInsertionInPreorder.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Node{
+
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d){
+        this -> data = d;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+
+};
+
+//NLR
+void preOrder(struct Node* root) {
+    //base case
+    if(root == NULL){
+        return;
+    }
+    cout << root->data <<" "; //Node
+    preOrder(root->left);     //Left
+    preOrder(root->right);    //Right
+}
+
+void levelOrderTraversal(Node*root) {
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL){
+            cout << endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout << temp -> data << " ";
+            if(temp -> left) {
+                q.push(temp->left);
+            }
+
+            if(temp->right) {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+Node* insertIntoBST(Node* root, int data) {
+    //base case
+    if(root == NULL){
+        root = new Node(data);
+        return root;
+    }
+    if(data > root->data) //insert in right part
+    {
+     root -> right = insertIntoBST(root -> right, data);
+    }
+    else  //insert in left part
+    {
+     root -> left = insertIntoBST(root->left,data);
+    }
+
+    return root;
+
+}
+//Function to take input 
+void takeInput(Node* &root) {
+     
+     int data;
+     cin >> data;
+
+     while(data != -1) {
+       root =  insertIntoBST(root,data);
+        cin >> data;
+     }
+}
+
+int main(){
+
+    Node* root = NULL;
+
+    cout << "Enter data for creating BST : " << endl;
+    takeInput(root);
+    
+    cout << "Printing the BST" << endl;
+    levelOrderTraversal(root);
+
+    cout << "Printing BST in Preorder : ";
+    preOrder(root); 
+
+    return 0;
+}
